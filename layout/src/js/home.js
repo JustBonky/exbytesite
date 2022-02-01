4d44d2d597fdabfd39bd824a7059ce73fd2f582b
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-02-01 16:40:16 +0300

    smooth-scrollbar lib added

--- layout/src/js/home.js
+++ layout/src/js/home.js
@@ -1,26 +1,61 @@
-const header = document.getElementById('site-header');
+// const header = document.getElementById('site-header');
 
-window.addEventListener('scroll', function (event) {
+// document.querySelector('.scroll-content').addEventListener('scroll', function (event) {
 
-    if (window.scrollY > 0) {
-        gsap.to(header, 0.2, {
-            height: 80,
-            color: '#000'
-        })
-        gsap.to('.header-bg', 0.15, {
-            opacity: 1,
-            y: '0%',
-            ease: Sine.easeOut
-        })
-    } else {
-        gsap.to(header, 0.3, {
-            height: 100,
-            color: '#fff'
-        })
-        gsap.to('.header-bg', {
-            duration: 0.3,
-            opacity: 0,
-            y: '-100%'
-        })
-    }
+//     console.log(event)
+
+//     // if (window.scrollY > 0) {
+//     //     gsap.to(header, 0.2, {
+//     //         height: 80,
+//     //         color: '#000'
+//     //     })
+//     //     gsap.to('.header-bg', 0.15, {
+//     //         opacity: 1,
+//     //         y: '0%',
+//     //         ease: Sine.easeOut
+//     //     })
+//     // } else {
+//     //     gsap.to(header, 0.3, {
+//     //         height: 100,
+//     //         color: '#fff'
+//     //     })
+//     //     gsap.to('.header-bg', {
+//     //         duration: 0.3,
+//     //         opacity: 0,
+//     //         y: '-100%'
+//     //     })
+//     // }
+// });
+
+var Scrollbar = window.Scrollbar;
+
+Scrollbar.init(document.body, {
+    delegateTo: document.querySelector('scroll-content')
 });
+
+document.querySelector('.scroll-content').addEventListener('wheel', function (event) {
+
+    console.log(event.clientY)
+
+    // if (event.clientY > 0) {
+    //     gsap.to(header, 0.2, {
+    //         height: 80,
+    //         color: '#000'
+    //     })
+    //     gsap.to('.header-bg', 0.15, {
+    //         opacity: 1,
+    //         y: '0%',
+    //         ease: Sine.easeOut
+    //     })
+    // } else {
+    //     gsap.to(header, 0.3, {
+    //         height: 100,
+    //         color: '#fff'
+    //     })
+    //     gsap.to('.header-bg', {
+    //         duration: 0.3,
+    //         opacity: 0,
+    //         y: '-100%'
+    //     })
+    // }
+})
